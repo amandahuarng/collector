2e68fca5fb34019e6cb466072a2d0ab00fa94796
amandahuarng <amandahuarng@gmail.com>
2020-11-11 19:02:03 -0800

    First commit

---
+++ blacklight.js
@@ -0,0 +1,57 @@
+const { collector } = require("@themarkup/blacklight-collector");
+const path = require("path");
+const {join} = require("path");
+const fs = require("fs");
+module.exports.scrapeSource = scrapeSource;
+async function scrapeSource(url) {
+  const EMULATE_DEVICE = false;
+ // Save the results to a folder
+  let OUT_DIR = true;
+
+  // The URL to test
+  let URL = url;
+  console.log(url)
+  //console.log(URL)
+  const defaultConfig = {
+    inUrl: `http://${URL}`,
+    numPages: 2,
+    headless: false,
+    emulateDevice: EMULATE_DEVICE
+  };
+  const result = await collector(
+    OUT_DIR
+      ? { ...defaultConfig, ...{ outDir: join(__dirname, "demo-dir") } }
+      : defaultConfig
+  )
+  if (OUT_DIR) {
+    console.log(
+      `For captured data please look in ${join(__dirname, "demo-dir")}`
+    );
+  }
+
+  const folderName = join(__dirname,"compiled",URL)
+
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+
+  const pathToFile = join(__dirname, "demo-dir", "inspection.json")
+  const pathToNewDestination = join(folderName, "inspection.json" )
+
+  fs.copyFile(pathToFile, pathToNewDestination, function (err) {
+    if (err) {
+      throw err
+    } else {
+      console.log("Successfully copied and moved the file!")
+    }
+  })
+  URL = null
+  return null;
+
+}
+
+module.exports = scrapeSource;
---
+++ collect.js
@@ -0,0 +1,79 @@
+var fs = require('fs')
+var path = require('path')
+var parse = require('csv-parse/lib/sync');
+
+const neatCsv = require('neat-csv');
+const scrapeSource = require('./blacklight.js');
+var body = null
+
+const filePath = path.join(__dirname, '/blacklight_remaining.csv');
+// //loading data from the file
+// var global_data = fs.readFileSync(filePath).toString();
+// //the dataset
+// const input = global_data;
+// //calling the npm package and save to records
+// const data = parse(input, {
+//     columns: true,
+//     skip_empty_lines: true
+// });
+
+// //map the output from csv-parse to the column
+// const urls = data.map(dat => dat["mecha.cc"]);
+// console.log(urls)
+
+// urls.forEach(async (url) => {
+//     const contents = await scrapeSource(url)
+//     console.log("finished scraping ", url)
+// })
+var start = async function() {
+    fs.readFile(filePath, async (error, data) => {
+        try{
+            const dataAsString = data.toString();
+            const dat = parse(dataAsString, {
+                        columns: true,
+                        skip_empty_lines: true
+            });
+
+            //map the output from csv-parse to the column
+            const urls = dat.map(da => da["mecha.cc"]);
+            const length = urls.length;
+            console.log(length)
+            for (let h = 0; h < length; h++){
+                url = urls[h]
+                //console.log(url)
+                const work = await scrapeSource(url)
+                console.log(work)
+                
+            }
+            
+            // urls.forEach( async (url) => {
+            //     access = await scrapeSource(url);
+            //     console.log(access)
+            //     // do something with item
+            // });
+
+            
+        } catch (e){
+            console.log(e)        }
+    })};
+
+start()
+
+
+
+
+
+
+// async function getUrls() {}
+//     let csv = await readFile(filePath);
+//     //let urls = await Promise(csv);
+//     console.log(urls)
+
+//     return new Promise((resolve, reject) => {
+//         scrapeSource(url, (result) => {
+//             resolve(result);
+//         })
+//     })
+// }
+
+// getUrls()
